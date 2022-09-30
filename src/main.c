847c17740aee57e92eee836623b1e2aa2920cd2c
David Pierre Sugar <david@thesugar.de>
2022-09-30 22:10:24 +0200

    everything replaced, except the main function

--- src/main.c
+++ src/main.c
@@ -56,23 +56,6 @@
  *   $ python3 hid_test.py
  */
 
-//--------------------------------------------------------------------+
-// MACRO CONSTANT TYPEDEF PROTYPES
-//--------------------------------------------------------------------+
-
-/* Blink pattern
- * - 250 ms  : device not mounted
- * - 1000 ms : device mounted
- * - 2500 ms : device is suspended
- */
-enum  {
-  BLINK_NOT_MOUNTED = 250,
-  BLINK_MOUNTED = 1000,
-  BLINK_SUSPENDED = 2500,
-};
-
-static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
-
 void led_blinking_task(void);
 
 /*------------- MAIN -------------*/
@@ -90,85 +73,4 @@ int main(void)
   }
 
   return 0;
-}
-
-//--------------------------------------------------------------------+
-// Device callbacks
-//--------------------------------------------------------------------+
-
-// Invoked when device is mounted
-void tud_mount_cb(void)
-{
-  blink_interval_ms = BLINK_MOUNTED;
-}
-
-// Invoked when device is unmounted
-void tud_umount_cb(void)
-{
-  blink_interval_ms = BLINK_NOT_MOUNTED;
-}
-
-// Invoked when usb bus is suspended
-// remote_wakeup_en : if host allow us  to perform remote wakeup
-// Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
-  (void) remote_wakeup_en;
-  blink_interval_ms = BLINK_SUSPENDED;
-}
-
-// Invoked when usb bus is resumed
-void tud_resume_cb(void)
-{
-  blink_interval_ms = BLINK_MOUNTED;
-}
-
-//--------------------------------------------------------------------+
-// USB HID
-//--------------------------------------------------------------------+
-
-// Invoked when received GET_REPORT control request
-// Application must fill buffer report's content and return its length.
-// Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
-{
-  // TODO not Implemented
-  (void) itf;
-  (void) report_id;
-  (void) report_type;
-  (void) buffer;
-  (void) reqlen;
-
-  return 0;
-}
-
-// Invoked when received SET_REPORT control request or
-// received data on OUT endpoint ( Report ID = 0, Type = 0 )
-/*
-void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
-{
-  // This example doesn't use multiple report and report ID
-  (void) itf;
-  (void) report_id;
-  (void) report_type;
-
-  // echo back anything we received from host
-  tud_hid_report(0, buffer, bufsize);
-}
-*/
-
-//--------------------------------------------------------------------+
-// BLINKING TASK
-//--------------------------------------------------------------------+
-void led_blinking_task(void)
-{
-  static uint32_t start_ms = 0;
-  static bool led_state = false;
-
-  // Blink every interval ms
-  if ( board_millis() - start_ms < blink_interval_ms) return; // not enough time
-  start_ms += blink_interval_ms;
-
-  board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
 }
--- src/zigusb.zig
+++ src/zigusb.zig
@@ -1,5 +1,11 @@
+//--------------------------------------------------------------------+
+// Extern
+//--------------------------------------------------------------------+
+
 // defined in tinyusb/src/class/hid/hid_device.h
 extern fn tud_hid_n_report(instance: u8, report_id: u8, report: [*]const u8, len: u16) bool;
+extern fn board_millis() u32;
+extern fn board_led_write(state: bool) void;
 
 const HidReportType = enum(c_int) {
     // defined in tinyusb/src/class/hid/hid.h
@@ -8,6 +14,10 @@ const HidReportType = enum(c_int) {
     output,
     feature,
 };
+
+//--------------------------------------------------------------------+
+// Wrapper
+//--------------------------------------------------------------------+
 
 fn tudHidReport(report_id: u8, report: []const u8) bool {
     if (report.len > 65535) {
@@ -18,13 +28,89 @@ fn tudHidReport(report_id: u8, report: []const u8) bool {
     return tud_hid_n_report(0, report_id, report.ptr, @intCast(u16, report.len));
 }
 
+//--------------------------------------------------------------------+
+// MACRO CONSTANT TYPEDEF PROTYPES
+//--------------------------------------------------------------------+
+
+/// Blink pattern
+/// - 250 ms    : device not mounted
+/// - 1000 ms   : device mounted
+/// - 2500 ms   : device is suspended
+const Blink = enum(u32) {
+    not_mounted = 250,
+    mounted = 1000,
+    suspended = 2500,
+};
+
+var blink_interval_ms: u32 = @enumToInt(Blink.not_mounted);
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+/// Invoked when device is mounted.
+export fn tud_mount_cb() void {
+    blink_interval_ms = @enumToInt(Blink.mounted);
+}
+
+// Invoked when device is unmounted.
+export fn tud_umount_cb() void {
+    blink_interval_ms = @enumToInt(Blink.not_mounted);
+}
+
+/// Invoked when usb bus is suspended
+/// remote_wakeup_en : if host allow us  to perform remote wakeup
+/// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+export fn tud_suspend_cb(remote_wakeup_en: bool) void {
+    _ = remote_wakeup_en;
+    blink_interval_ms = @enumToInt(Blink.suspended);
+}
+
+// Invoked when usb bus is resumed.
+export fn tud_resume_cb() void {
+    blink_interval_ms = @enumToInt(Blink.mounted);
+}
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+
+/// Invoked when received SET_REPORT control request or
+/// received data on OUT endpoint ( Report ID = 0, Type = 0 ).
 export fn tud_hid_set_report_cb(itf: u8, report_id: u8, report_type: HidReportType, buffer: [*]u8, bufsize: u16) void {
     _ = itf;
     _ = report_id;
     _ = report_type;
-    _ = buffer;
-    _ = bufsize;
 
-    //_ = tud_hid_n_report(0, 0, buffer, bufsize);
-    _ = tudHidReport(0, "fuck you bitch!");
+    _ = tudHidReport(0, buffer[0..bufsize]);
+}
+
+// Invoked when received GET_REPORT control request.
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request.
+export fn tud_hid_get_report_cb(itf: u8, report_id: u8, report_type: HidReportType, buffer: [*]u8, reqlen: u16) u16 {
+    _ = itf;
+    _ = report_id;
+    _ = report_type;
+    _ = buffer;
+    _ = reqlen;
+
+    return 0;
+}
+
+//--------------------------------------------------------------------+
+// BLINKING TASK
+//--------------------------------------------------------------------+
+
+export fn led_blinking_task() void {
+    const S = struct {
+        var start_ms: u32 = 0;
+        var led_state: bool = false;
+    };
+
+    if (board_millis() - S.start_ms < blink_interval_ms) return;
+    S.start_ms += blink_interval_ms;
+
+    board_led_write(S.led_state);
+    S.led_state = !S.led_state;
 }
