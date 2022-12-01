6d33b4cf174a6f0f0765d4e5395024d36aa5e1d3
David P. Sugar <david@thesugar.de>
2022-12-01 18:07:17 +0100

    tool scripts updated

--- tools/assert.sh
+++ tools/assert.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+if [[ $# -lt 1 ]]; then
+    echo "usage: $0 <hidraw-id>"
+    exit 1
+fi
+
 echo assertion challenge | openssl sha256 -binary | base64 > assert_param
 echo relying party >> assert_param
 head -1 cred >> assert_param
 tail -n +2 cred > pubkey
 
-fido2-assert -G -i assert_param /dev/hidraw6 | fido2-assert -V pubkey es256
+fido2-assert -G -i assert_param /dev/hidraw$1 | fido2-assert -V pubkey es256
--- tools/cred.sh
+++ tools/cred.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+if [[ $# -lt 1 ]]; then
+    echo "usage: $0 <hidraw-id>"
+    exit 1
+fi
+
 echo credential challenge | openssl sha256 -binary | base64 > cred_param
 echo relying party >> cred_param
 echo franzi >> cred_param
 dd if=/dev/urandom bs=1 count=32 | base64 >> cred_param
 
-fido2-cred -M -i cred_param /dev/hidraw6 | fido2-cred -V -o cred
+fido2-cred -M -i cred_param /dev/hidraw$1 | fido2-cred -V -o cred
 
 cat cred
